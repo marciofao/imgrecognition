9d3a7529ea21a7734e431e4d64d3d7ee933d212a
Marcio Fao <marcio.lopes.fao@gmail.com>
2026-03-29 16:49:04 -0300

    init

---
+++ src/main/java/com/example/imgrecognition/ImgrecognitionApplication.java
@@ -0,0 +1,13 @@
+package com.example.imgrecognition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImgrecognitionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImgrecognitionApplication.class, args);
+	}
+
+}
